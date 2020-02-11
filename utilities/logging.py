dc0b5d96ba1978426742efb978c7f89266cc1219
YoungYoung619 <25259590+YoungYoung619@users.noreply.github.com>
2020-02-11 22:17:13 +0800

    'init'

---
+++ utilities/logging.py
@@ -0,0 +1,9 @@
+"""
+Copyright (c) College of Mechatronics and Control Engineering, Shenzhen University.
+All rights reserved.
+Description :
+Author：Team Li
+"""
+import logging
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
